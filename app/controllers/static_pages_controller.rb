13877bed70664342b67d83ff3899635750feafb6
Aaron Tree <jiujiubad@gmail.com>
2019-03-27 11:45:50 +0800

    Add basic static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def course_intro
+  end
+
+  def teachers
+  end
+
+  def students
+  end
+
+  def faq
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/course_intro', to: 'static_pages#course_intro'
+  get    '/teachers',     to: 'static_pages#teachers'
+  get    '/students',     to: 'static_pages#students'
+  get    '/faq',          to: 'static_pages#faq'
+  get    '/contact',      to: 'static_pages#contact'
+  get    '/signup',       to: 'users#new'
+  get    '/login',        to: 'sessions#new'
+  post   '/login',        to: 'sessions#create'
+  delete '/logout',       to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
